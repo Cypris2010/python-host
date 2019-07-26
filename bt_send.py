a29a36a315817572ae695d1d9b87a3617252aad4
Fabian Bott <fabianbott@Fabians-Mini.fritz.box>
2019-07-26 12:13:25 +0200

    changes order

--- bt_send.py
+++ bt_send.py
@@ -1,7 +1,7 @@
 from gattlib import GATTRequester
 from time import sleep
 
+sleep(2.0)
 req = GATTRequester("CF:EC:C9:D4:85:39", False)
 req.connect(True, 'random')
-sleep(2000.00)
 req.write_by_handle(0x16, b"\x57\x01\x00")
