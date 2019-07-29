bc6e27591628069ed842419078d4c42d9dd2537f
Fabian Bott <fabianbott@gmail.com>
2019-07-29 08:35:05 +0200

    Changes to be committed: 	new file:   archive.tar 	modified:   bt_send.py

--- bt_send.py
+++ bt_send.py
@@ -2,6 +2,6 @@ from gattlib import GATTRequester
 from time import sleep
 
 sleep(2.0)
-req = GATTRequester("CF:EC:C9:D4:85:39", False)
+req = GATTRequester("F0:18:98:A6:FC:73", False)
 req.connect(True, 'random')
 req.write_by_handle(0x16, b"\x57\x01\x00")
